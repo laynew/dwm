7e0068ea3bf415009f8dce68f1562ef6d037197a
Layne Wedderburn <layne@wedderburn.co.za>
2024-07-24 06:14:38 +0100

    Change bar colour to blue

--- config.def.h
+++ config.def.h
@@ -24,7 +24,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#FF8800";
+static const char col_cyan[]        = "#1e73b5";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
