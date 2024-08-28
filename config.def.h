92275b386398694758f87fe4e568d70f28b0b4e9
Layne Wedderburn <layne@wedderburn.co.za>
2024-08-28 14:11:38 +0100

    Add Noto Color Emoji font

--- config.def.h
+++ config.def.h
@@ -18,7 +18,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "DejaVuSansMono:size=13", "monospace:size=13" };
+static const char *fonts[]          = { "DejaVuSansMono:size=13", "monospace:size=13", "Noto Color Emoji:size=10" };
 static const char dmenufont[]       = "DejaVuSansMono:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
