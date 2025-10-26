287fadaab3a11e46ab156ef1c92545b77f7837cc
Layne Wedderburn <layne@wedderburn.co.za>
2025-10-26 20:49:13 +0000

    Fix pertag/taglayout

--- dwm.c
+++ dwm.c
@@ -829,6 +829,9 @@ createmon(void)
 	m->gappiv = gappiv;
 	m->gappoh = gappoh;
 	m->gappov = gappov;
+	m->lt[0] = &layouts[0];
+	m->lt[1] = &layouts[1 % LENGTH(layouts)];
+	strncpy(m->ltsymbol, layouts[0].symbol, sizeof m->ltsymbol);
 	m->pertag = ecalloc(1, sizeof(Pertag));
 	m->pertag->curtag = m->pertag->prevtag = 1;
 
