8362e21f81ddea9c9db59aee74e61ff7de0d0c58
Layne Wedderburn <layne@wedderburn.co.za>
2025-10-26 10:58:33 +0000

    Add custom commands and keybinds
    Add commands+keybinds for:
    - password menu
    - volume
    - brightness
    - screen capture
    
    Use MOD+r for dmenu launcher

--- config.h
+++ config.h
@@ -106,15 +106,34 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+#include <X11/XF86keysym.h>
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *keepass_password_menucmd[] = { "keepassxc-menu" };
+static const char *keepass_totp_menucmd[] = { "keepassxc-menu", "--totp" };
+static const char *keepass_username_menucmd[] = { "keepassxc-menu", "--name" };
 static const char *termcmd[]  = { "st", NULL };
+static const char *backlight_up[]   = { "cmd-adjust-backlight", "+15", NULL };
+static const char *backlight_down[] = { "cmd-adjust-backlight", "-15", NULL };
+static const char *vol_up[]   = { "cmd-volume-set", "5%+", NULL };
+static const char *vol_down[] = { "cmd-volume-set", "5%-", NULL };
+static const char *vol_mute[] = { "cmd-volume-set", "toggle", NULL };
+static const char *capture_screen[] = { "cmd-screenshot-to-clipboard", NULL };
 
 #include "exitdwm.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ 0,                            XF86XK_MonBrightnessUp, spawn, {.v = backlight_up } },
+	{ 0,                            XF86XK_MonBrightnessDown, spawn, {.v = backlight_down } },
+	{ 0,                            XF86XK_AudioMute, spawn,   {.v = vol_mute } },
+	{ 0,                            XF86XK_AudioLowerVolume, spawn, {.v = vol_down } },
+	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = vol_up } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = capture_screen } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = keepass_password_menucmd } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = keepass_totp_menucmd } },
+	{ MODKEY|ShiftMask|ControlMask, XK_p,      spawn,          {.v = keepass_username_menucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
