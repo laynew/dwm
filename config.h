6b41cccd05762c3d9834eadc18b1f0fe7d49d9c3
Layne Wedderburn <layne@wedderburn.co.za>
2025-10-26 10:53:50 +0000

    Set exitmenu to Mod+Backspace

--- config.h
+++ config.h
@@ -170,7 +170,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
-	{ MODKEY|ShiftMask,             XK_e,      exitdwm,       {0} },
+	{ MODKEY,                       XK_BackSpace,exitdwm,      {0} },
 };
 
 /* button definitions */
